d487ea929ba69bf43962c4f17d847e485c209eae
hayatonasu <hayatonasu1129@gmail.com>
2023-10-16 23:18:27 +0900

    Japanese pages.

---
+++ mathjax_setting.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+    TeX: { equationNumbers: { autoNumber: "AMS" }},
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      processEscapes: true
+    },
+    "HTML-CSS": { matchFontHeight: false },
+    displayAlign: "left",
+    displayIndent: "2em"
+  };
